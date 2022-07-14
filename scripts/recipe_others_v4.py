cd672d2b53bf8c9b3560becb8cb54537051332f4
unknown <rickrebel@gmail.com>
2022-07-14 07:47:01 -0500

    Reorganizo y recupero los distintos scripts de limpieza y tranformación

---
+++ scripts/recipe_others_v4.py
@@ -0,0 +1,71 @@
+
+
+
+def explore_first_time(file, group_file):
+    import pathlib
+    print(pathlib.Path('yourPath.example').suffix) # '.example'
+    print(pathlib.Path("hello/foo.bar.tar.gz").suffixes) # ['.bar', '.tar', '.gz']
+    #REVISAR:
+    suffixes = pathlib.Path(file.file_name).suffixes
+    format_file = group_file.format_file
+    if format_file == 'txt' or 'csv'
+
+
+
+
+def get_data_from_file(reporte_recetas_path):
+    import io
+    try:
+        with io.open(reporte_recetas_path, "r", encoding="latin-1") as file:
+            data = file.read()
+            file.close()
+    except Exception as e:
+        print(e)
+        return False, [u"%s" % (e)], False
+
+    rr_data_rows = data.split("\n")
+
+    return rr_data_rows, []
+
+
+
+def get_data_from_file(
+        reporte_recetas_path, clean_data=True):
+    import io
+
+    with_coma = False
+    from_excel = False
+    try:
+        # with open(reporte_recetas_path) as file:
+        #with io.open(reporte_recetas_path, "r", encoding="latin-1") as file:
+        with io.open(reporte_recetas_path, "r", encoding="latin-1") as file:
+            data = file.read()
+            if "|" not in data[:3000]:
+                with_coma = True
+            if "," not in data[:5000] and "|" not in data[:5000]:
+                print("-------------------")
+                print('empty %s' % reporte_recetas_path)
+                return False, ['empty %s' % reporte_recetas_path], False
+            if ",,," in data[:5000]:
+                from_excel = True
+
+            if clean_data:
+                if with_coma:
+                    data = special_coma(data)
+                if from_excel:
+                    data = special_excel(data)
+                data = data.replace("\"", "").replace(first_null, "")
+                data = clean_special(data)
+            file.close()
+    except Exception as e:
+        return False, [u"%s" % (e)], False
+
+    rr_data_rows = data.split(sep_rows)
+    if not with_coma:
+        headers = rr_data_rows.pop(0)
+        print(u"se quitó headers del procesado: ", headers)
+
+    return rr_data_rows, [], with_coma
+
+
+
