7aa92d18fc2dd49f9693bb032ad76245a3eb2101
unknown <rickrebel@gmail.com>
2022-07-26 09:11:32 -0500

    inicios de función de agregado por mes

--- catalog/api/views.py
+++ catalog/api/views.py
@@ -24,18 +24,18 @@ class EntityViewSet(ListRetrieveUpdateMix):
     permission_classes = (permissions.AllowAny,)
     serializer_class = serializers.EntitySerializer
     queryset = Entity.objects.all().prefetch_related(
-                "petitions",
-                "petitions__petition_months",
-                "petitions__file_groups",
-                "petitions__file_groups__group_file",
-                "petitions__file_groups__group_file__group_data",
-                "petitions__file_groups__group_file__type_file",
-                "petitions__file_groups__group_file__group_tranformations",
-                "petitions__file_groups__group_file__group_tranformations__clean_function",
-                "petitions__file_groups__data_files",
-                "petitions__file_groups__data_files__status_process",
-                "petitions__file_groups__data_files__month_entity",
-            )
+            "petitions",
+            "petitions__petition_months",
+            "petitions__file_groups",
+            "petitions__file_groups__group_file",
+            "petitions__file_groups__group_file__group_data",
+            "petitions__file_groups__group_file__type_file",
+            "petitions__file_groups__group_file__group_tranformations",
+            "petitions__file_groups__group_file__group_tranformations__clean_function",
+            "petitions__file_groups__data_files",
+            "petitions__file_groups__data_files__status_process",
+            "petitions__file_groups__data_files__month_entity",
+        )
     
     action_serializers = {
         "list": serializers.EntitySerializer,
@@ -49,6 +49,43 @@ class EntityViewSet(ListRetrieveUpdateMix):
             entity, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
         return Response()
+
+    """ @action(methods=["post"], detail=True, url_path='create_months')
+    def create_months(self, request, **kwargs):
+        import json
+        from files_rows.models import MonthEntity
+        if not request.user.is_staff:
+            raise PermissionDenied()
+        limiters = request.data.get("limiters")
+        limiters = json.loads(limiters)
+        entity = self.get_object()
+        
+        MonthEntity.objects.create()
+        errors = []
+
+        if pending in [True, "true"]:
+            report.pending = True
+        elif pending in [False, "false"]:
+            report.pending = False
+        elif pending:
+            errors.append("pending invalido")
+
+        if validated in [True, "true"]:
+            report.validated = True
+        elif validated in [False, "false"]:
+            report.validated = False
+        elif validated in ["null"]:
+            report.validated = None
+        elif validated:
+            errors.append("validated invalido")
+
+        if errors:
+            return Response({"errors": errors},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        report.save()
+
+        return Response(status=status.HTTP_202_ACCEPTED)"""
 
 
 class InstitutionList(ListMix):
