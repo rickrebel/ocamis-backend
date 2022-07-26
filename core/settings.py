d505686eee991633ab983689391eb51d74ac8338
unknown <rickrebel@gmail.com>
2022-07-26 14:07:35 -0500

    probando commit sin new_apps

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     "email_sendgrid.apps.EmailSendgridConfig",
     'ckeditor',
     'rest_framework.authtoken',
+    
     #"formula.apps.FormulaConfig",
     #"data_param.apps.DataParamConfig",
     #"inai.apps.InaiConfig",
